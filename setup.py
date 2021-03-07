4f010862e45eab6943452b773e4af31812044d54
Atreya Goswami <64495311+atreya221@users.noreply.github.com>
2021-03-07 20:21:34 +0530

    Configured setup.py to run on Windows

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 import os
-os.system('sudo apt-get install python3-venv')
-os.system('python3 -m pip install --upgrade pip')
-os.system('python3 -m pip install virtualenv')
-os.system('python3 -m venv .env')
-os.system('. .env/bin/activate; pip install --upgrade pip setuptools wheel')
-os.system('. .env/bin/activate; pip install -r requirements.txt')
+import platform
+if platform.system() == 'Linux':
+  os.system('sudo apt-get install python3-venv')
+  os.system('python3 -m pip install --upgrade pip')
+  os.system('python3 -m pip install virtualenv')
+  os.system('python3 -m venv .env')
+  os.system('. .env/bin/activate; pip install --upgrade pip setuptools wheel')
+  os.system('. .env/bin/activate; pip install -r requirements.txt')
+
+if platform.system() == 'Windows':
+  os.system('python -m pip install --upgrade pip')
+  os.system('python -m pip install virtualenv')
+  os.system('python -m venv .env')
+  os.system('.\.env\Scripts\\activate & pip install --upgrade pip setuptools wheel')
+  os.system('.\.env\Scripts\\activate & pip install -r requirements.txt')
