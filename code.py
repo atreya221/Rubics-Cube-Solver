32adef909ef8fb9e35b14474cf8b3aeecd54a948
Atreya Goswami <atreyagoswami@gmail.com>
2021-04-01 11:06:16 +0530

    Updated code module to scan all faces

--- code.py
+++ code.py
@@ -50,3 +50,142 @@ def main():
     except:
         print("Output output can't be created: %s" % file_name)
         sys.exit()
+
+    while True:
+        ok, frame = output.read()
+        if not ok:
+            break
+        while True:
+            #print("Show Front Face")
+            FF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Front Face")
+            midfront = FF[0,4]
+            print(FF)
+            print(midfront)
+            #print("Show Up Face")
+            #time.sleep(2)
+            UF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Top Face")
+            begin_time = datetime.now()
+            while True:
+                if (datetime.now() - begin_time).total_seconds() > 5:
+                    break
+                else:
+                    ok, frame = output.read()
+                    if not ok:
+                        flag = 1
+                        break
+                    frame = cv2.putText(frame, "Show Front Face", (40, 40), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 255), 4)
+                    outputWriter.write(frame)
+                    cv2.imshow("Video Output", frame)
+                    key_pressed = cv2.waitKey(1) & 0xFF
+                    if key_pressed == 27 or key_pressed == ord('q'):
+                        flag = 1
+                        break
+                    
+            if flag == 1:
+                sys.exit()
+                
+            midup = UF[0, 4]
+            print(UF)
+            print(midup)
+            #print("Show Down Face")
+            #time.sleep(2)
+            DF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Down Face")
+            begin_time = datetime.now()
+            while True:
+                if (datetime.now() - begin_time).total_seconds() > 5:
+                    break
+                else:
+                    ok, frame = output.read()
+                    if not ok:
+                        flag = 1
+                        break
+                    frame = cv2.putText(frame, "Show Top Face", (40, 40), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 255), 4)
+                    outputWriter.write(frame)
+                    cv2.imshow("Video Output", frame)
+                    key_pressed = cv2.waitKey(1) & 0xFF
+                    if key_pressed == 27 or key_pressed == ord('q'):
+                        flag = 1
+                        break
+            if flag == 1:
+                sys.exit()
+            middown = DF[0, 4]
+            print(DF)
+            print(middown)
+            #print("Show Right Face")
+            #time.sleep(2)
+            RF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Right Face")
+            begin_time = datetime.now()
+            while True:
+                if (datetime.now() - begin_time).total_seconds() > 5:
+                    break
+                else:
+                    ok, frame = output.read()
+                    if not ok:
+                        flag = 1
+                        break
+                    frame = cv2.putText(frame,"Show Down Face", (40, 40), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 255), 4)
+                    outputWriter.write(frame)
+                    cv2.imshow("Video Output", frame)
+                    key_pressed = cv2.waitKey(1) & 0xFF
+                    if key_pressed == 27 or key_pressed == ord('q'):
+                        flag = 1
+                        break
+            if flag == 1:
+                sys.exit()
+            midright = RF[0, 4]
+            print(RF)
+            print(midright)
+            #print("Show Left Face")
+            #time.sleep(2)
+            LF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Left Face")
+            begin_time = datetime.now()
+            while True:
+                if (datetime.now() - begin_time).total_seconds() > 5:
+                    break
+                else:
+                    ok, frame = output.read()
+                    if not ok:
+                        flag = 1
+                        break
+                    frame = cv2.putText(frame, "Show Right Face", (40, 40), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 255), 4)
+                    outputWriter.write(frame)
+                    cv2.imshow("Video Output", frame)
+                    key_pressed = cv2.waitKey(1) & 0xFF
+                    if key_pressed == 27 or key_pressed == ord('q'):
+                        flag = 1
+                        break
+            if flag == 1:
+                sys.exit()
+            midleft = LF[0, 4]
+            print(LF)
+            print(midleft)
+            #print("Show Back Face")
+            #time.sleep(2)
+            BF = form_face.form_face(output, outputWriter, UF, RF, FF, DF, LF, BF, caption="Show Back Face")
+            begin_time = datetime.now()
+            while True:
+                if (datetime.now() - begin_time).total_seconds() > 5:
+                    break
+                else:
+                    ok, frame = output.read()
+                    if not ok:
+                        flag = 1
+                        break
+                    frame = cv2.putText(frame, "Show Left Face", (40, 40), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 255), 4)
+                    outputWriter.write(frame)
+                    cv2.imshow("Video Output", frame)
+                    key_pressed = cv2.waitKey(1) & 0xFF
+                    if key_pressed == 27 or key_pressed == ord('q'):
+                        flag = 1
+                        break
+            if flag == 1:
+                sys.exit()
+            midback = BF[0, 4]
+            print(BF)
+            #time.sleep(2)
+            print(midback)
+
+            unsolved_cube = concat(UF,RF,FF,DF,LF,BF)
+
+if __name__ == "__main__":
+    main()
